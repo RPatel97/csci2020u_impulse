4925b010f090d71ecc94db54de9809afb2636f23
Rishabh Patel <sam.rishabh.patel@gmail.com>
2017-01-23 12:49:21 -0500

    Added lab02

--- lab02/src/main/java/csci2020u/lab02/SimpleTest.java
+++ lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -1,7 +1,12 @@
 package csci2020u.lab02;
 
 public class SimpleTest {
-	public static void main(String[] args) {
-		System.out.println("Test");
-	}
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
 }
