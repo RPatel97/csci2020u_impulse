c007a2305462eca82cea9d432e95862278034884
Rishabh Patel <sam.rishabh.patel@gmail.com>
2017-01-30 12:56:40 -0500

    Adding files for lab03

---
+++ lab03/src/main/java/csci2020u/lab03/SimpleTest.java
@@ -0,0 +1,12 @@
+package csci2020u.lab03;
+
+public class SimpleTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
